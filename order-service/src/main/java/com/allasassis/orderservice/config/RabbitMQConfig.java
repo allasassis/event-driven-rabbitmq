7cf80793a5b095a9c0eabb73716b044a6b5b6300
Allas Assis <allasassis10@gmail.com>
2024-02-24 15:54:12 -0300

    main: RabbitMQ Config class created

--- order-service/src/main/java/com/allasassis/orderservice/config/RabbitMQConfig.java
+++ order-service/src/main/java/com/allasassis/orderservice/config/RabbitMQConfig.java
@@ -1,9 +1,50 @@
 package com.allasassis.orderservice.config;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    
+    @Value("${rabbitmq.queue.order.name}")
+    private String orderQueue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String orderExchange;
+
+    @Value("${rabbitmq.binding.routing.key}")
+    private String orderRoutingKey;
+
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(orderQueue);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(orderExchange);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(orderQueue()).to(exchange()).with(orderRoutingKey);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
 }
